e02f5b2c69662ae0c03cceda4f13f2730f1d94fb
antmx <antmx1@ntlworld.com>
2018-04-13 22:08:17 +0100

    Deleted JS files that are auto-generated from TS

--- Site/scripts/custom/CreateDbIndexArgs.js
+++
@@ -1,9 +0,0 @@
-var CreateDbIndexArgs = /** @class */ (function () {
-    function CreateDbIndexArgs(indexName, keyPath, optionalParameters) {
-        this.IndexName = indexName;
-        this.KeyPath = keyPath;
-        this.OptionalParameters = optionalParameters;
-    }
-    return CreateDbIndexArgs;
-}());
-//# sourceMappingURL=CreateDbIndexArgs.js.map
